1b73ac518beba5e27bfc152096686238fba579ad
Jorden <jakereps@users.noreply.github.com>
2017-01-09 21:48:08 -0700

    MAINT: Prune files

--- test/test-spec.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-expressions*/
-/* global describe it */
-import { expect } from 'chai';
-
-describe('placeholder tests...', () => {
-  it('passes all tests!', () => {
-    expect(1).to.equal(1);
-  });
-});
